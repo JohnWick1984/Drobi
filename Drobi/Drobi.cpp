6c3f331b0af963e5266c725eb0f26b47fe1369bc
Евгений <@EUGENE1984>
2023-04-08 13:14:58 +0500

    Добавьте файлы проекта.

---
+++ Drobi/Drobi.cpp
@@ -0,0 +1,100 @@
+﻿// Drobi.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+
+class Fraction {
+private:
+    int numerator;
+    int denominator;
+
+public:
+    Fraction(int numerator = 0, int denominator = 1) : numerator(numerator), denominator(denominator) {}
+
+    void input_fraction()
+    {
+        cout << "Введите числитель: ";
+        cin >> numerator;
+        cout << "Введите знаменатель: ";
+        cin >> denominator;
+    }
+
+    int gcd(int a, int b)
+    {
+        if (b == 0) 
+        {
+            return a;
+        }
+        else 
+        {
+            return gcd(b, a % b);
+        }
+    }
+
+    void simplify_fraction() 
+    {
+        int gcd_num = gcd(numerator, denominator);
+        numerator /= gcd_num;
+        denominator /= gcd_num;
+    }
+
+    Fraction operator+(const Fraction& other) 
+    {
+        Fraction result(numerator * other.denominator + other.numerator * denominator, denominator * other.denominator);
+        result.simplify_fraction();
+        return result;
+    }
+
+    Fraction operator-(const Fraction& other) 
+    {
+        Fraction result(numerator * other.denominator - other.numerator * denominator, denominator * other.denominator);
+        result.simplify_fraction();
+        return result;
+    }
+
+    Fraction operator*(const Fraction& other) 
+    {
+        Fraction result(numerator * other.numerator, denominator * other.denominator);
+        result.simplify_fraction();
+        return result;
+    }
+
+    Fraction operator/(const Fraction& other) 
+    {
+        Fraction result(numerator * other.denominator, denominator * other.numerator);
+        result.simplify_fraction();
+        return result;
+    }
+
+    void print_fraction()
+    {
+        cout << numerator << "/" << denominator << endl;
+    }
+};
+
+int main()
+{
+    Fraction a, b, c, d, e;
+
+    a.input_fraction();
+    b.input_fraction();
+
+    c = a + b;
+    d = a - b;
+    e = a * b;
+
+    cout << "a + b = ";
+    c.print_fraction();
+
+    cout << "a - b = ";
+    d.print_fraction();
+
+    cout << "a * b = ";
+    e.print_fraction();
+
+    cout << "a / b = ";
+    (a / b).print_fraction();
+
+    return 0;
+}
---
+++ klass string/klass string.cpp
@@ -0,0 +1,55 @@
+﻿// klass string.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <string>
+using namespace std;
+class String 
+{
+public:
+    static int getCount() { return count; }
+
+    String() : String(80) {}
+    String(int size) : length(size) { data = new char[size]; count++; }
+    String(const char* str) : String(strlen(str)) 
+    {
+        memcpy(data, str, length);
+    }
+
+    ~String() { delete[] data; count--; }
+
+    void input() 
+    {
+        cin.getline(data, length);
+    }
+
+    void output() const
+    {
+        cout << data << endl;
+    }
+
+private:
+    static int count;
+    char* data;
+    int length;
+};
+
+int String::count = 0;
+
+int main() 
+{
+    String s1; 
+    s1.input(); 
+    s1.output(); 
+
+    String s2(50); 
+    s2.input();
+    s2.output();
+
+    String s3("Привет!"); 
+    s3.output();
+
+    cout << "Количество созданных строк: " << String::getCount() << endl;
+
+    return 0;
+}
